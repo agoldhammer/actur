0bc1dc622e74b82afae552d30626583ac0b2f7d9
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-04 19:40:39 -0400

    trying out parser

--- actur/actur.py
+++ actur/actur.py
@@ -1,1 +1,9 @@
 """Main module."""
+import feedparser
+import pprint
+
+d = feedparser.parse("https://feedparser.readthedocs.io/en/latest/examples/atom10.xml")
+# print(d)
+print(d["feed"]["title"])
+print(d["feed"]["updated"])
+pprint.pprint(d)
