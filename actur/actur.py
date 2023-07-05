ef9152c2a24ed26714a88b58437ed1c6f4abc14d
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-05 14:05:41 -0400

    experiment

--- actur/actur.py
+++ actur/actur.py
@@ -2,8 +2,41 @@
 import feedparser
 import pprint
 
-d = feedparser.parse("https://feedparser.readthedocs.io/en/latest/examples/atom10.xml")
+from feeds import lemonde, sz
+
+current_feed = sz
+
+name = current_feed.name
+feed = current_feed.feeds[0]
+feedname = feed.name
+url = feed.url
+
+d = feedparser.parse(url)
+print("version", d.version)
 # print(d)
-print(d["feed"]["title"])
-print(d["feed"]["updated"])
-pprint.pprint(d)
+# print(d["feed"]["title"])
+# print(d["feed"]["updated"])
+# print(d.feed.title)
+print("Pubname", name)
+print("Feed:", feedname)
+# print("channel title", d.feed.keys())
+# print("d.channel", d.channel.keys())
+# pprint.pprint(d)
+print("d keys", d.keys())
+print("no. entries", len(d.entries))
+for entry in d.entries:
+    pprint.pprint(entry)
+
+print("channel by items")
+print("channel title", d.channel.title)
+for item in d.channel.items():
+    pprint.pprint(item)
+# print("channel links\n")
+# pprint.pprint(d.channel.links)
+# print("namespaces", d.namespaces)
+# print("feedkeys")
+# pprint.pprint(d.feed.keys())
+# print("feed links", d.feed.links)
+# print("feed summary", d.feed.summary)
+# print(d.updated)
+# print(d.channel.description)
---
+++ actur/feeds.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Feed:
+    name: str
+    url: str
+
+
+@dataclass
+class Publication:
+    name: str
+    feeds: List[Feed]
+
+
+lemonde = Publication(
+    name="LeMonde",
+    feeds=[Feed("Politique", "https://www.lemonde.fr/politique/rss_full.xml)")],
+)
+
+sz = Publication(
+    name="SZ", feeds=[Feed("TopThemen", "https://rss.sueddeutsche.de/rss/Topthemen")]
+)
