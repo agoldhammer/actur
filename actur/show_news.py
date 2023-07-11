656eb80a91b98726df336931238a4ae56cd6315d
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-11 14:33:23 -0400

    fixed option for all pubs

--- actur/show_news.py
+++ actur/show_news.py
@@ -3,17 +3,17 @@ import sys
 
 import click
 
-from actur.utils import dbif, display, query
+from actur.utils import dbif, display, feeds, query
 
 
 @click.command()
 @click.option("--pubname", "-p", multiple=True, default=["all"], help="pubname")
 @click.option("--start", "-s", help="start date")
-@click.option("--end", "-e", help="endate")
+@click.option("--end", "-e", help="end date")
 @click.option("--days", "-d", type=int, help="days ago")
 @click.option("--hours", "-h", type=int, help="hours ago")
 @click.option("--summ/--no-summ", default=True)
-def main(pubname: str, start: str, end: str, summ: bool, days: int, hours: int):
+def main(pubname: list[str], start: str, end: str, summ: bool, days: int, hours: int):
     """Console script for actur."""
     dbif.init_db("mongodb://elite.local")
     if days is not None or hours is not None:
@@ -24,12 +24,9 @@ def main(pubname: str, start: str, end: str, summ: bool, days: int, hours: int):
         # pubname is a list, since it may be specified multiple times
         # on command line
         if "all" in pubname:
-            print("want to output all pubs")
-        else:
-            articles = db.daterange.find({"pubname": {"$in": pubname}}).sort(
-                "pubdate", 1
-            )
-            display.display_articles(articles)
+            pubname = [pub.name for pub in feeds.get_papers()]
+        articles = db.daterange.find({"pubname": {"$in": pubname}}).sort("pubdate", 1)
+        display.display_articles(articles)
 
     return 0
 
