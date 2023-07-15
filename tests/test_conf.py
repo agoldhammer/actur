7f08a13f8ec2b3720b5abefcb0d53f38ba510527
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-15 19:26:47 -0400

    added test of feed parsing

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -1,15 +1,25 @@
 from actur.config import readconf
-from actur.utils import dbif
+from actur.utils import dbif, feeds
 
-# import pytest
-
-
-# @pytest.fixture()
-# def setup():
-#     dbif.init_db()
+import pytest
 
 
-def test_conf_by_key():
+@pytest.fixture(autouse=True, scope="module")
+def setup():
     dbif.init_db()
+    yield
+
+
+def test_config_and_feedparsing():
+    # test setting of database
     res = readconf.get_conf_by_key("database")
     assert "dbname" in res  # nosec
+    assert "url" in res  # nosec
+    # test that Publication dataclasses are returned
+    rawpubs = readconf.get_conf_by_key("Publications")
+    names = [pub["name"] for pub in rawpubs]
+    assert "LeMonde" in names  # nosec
+    #  test transformation of rawpubs into Publication dataclasses
+    pubs = feeds.get_publications()
+    names = [pub.name for pub in pubs]
+    assert "SZ" in names  # nosec
