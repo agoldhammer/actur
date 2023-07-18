9ff2ba83bd8cc49ee116ac20da3825d343b79056
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-18 17:05:40 -0400

    added test_show_list

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,22 +7,6 @@ from click.testing import CliRunner
 from actur import actu_cli
 
 
-# @pytest.fixture
-# def response():
-#     """Sample pytest fixture.
-
-#     See more at: http://doc.pytest.org/en/latest/fixture.html
-#     """
-#     # import requests
-#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-# def test_content(response):
-#     """Sample pytest test function with the pytest fixture as an argument."""
-#     # from bs4 import BeautifulSoup
-#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
@@ -33,3 +17,9 @@ def test_command_line_interface():
     assert help_result.exit_code == 0  # nosec
     # print(help_result.output)
     assert "--help" in help_result.output  # nosec
+
+
+def test_show_list(mocker):
+    runner = CliRunner()
+    res = runner.invoke(actu_cli.cli, ["show", "--list"])
+    assert res.exit_code == 0  # nosec B101
