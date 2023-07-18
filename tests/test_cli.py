aa0d46b76f5858648e6a57f80379385cbbb4030c
Art Goldhammer <art.goldhammer@gmail.com>
2023-07-18 17:09:20 -0400

    added a test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,7 +19,8 @@ def test_command_line_interface():
     assert "--help" in help_result.output  # nosec
 
 
-def test_show_list(mocker):
+def test_show_list():
     runner = CliRunner()
     res = runner.invoke(actu_cli.cli, ["show", "--list"])
+    assert "LeMonde" in res.output  # nosec B101
     assert res.exit_code == 0  # nosec B101
