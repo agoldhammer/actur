2e4a66874841ee1174d9d7350fed9e0c262d1ba4
Art Goldhammer <art.goldhammer@gmail.com>
2024-08-07 18:01:15 +0000

    changed actur/__init__.py and requirements_dev

--- actur/__init__.py
+++ actur/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for actur."""
 
 __author__ = """Art Goldhammer"""
-__email__ = 'art.goldhammer@gmail.com'
-__version__ = '0.1.0'
+__email__ = "art.goldhammer@gmail.com"
+__version__ = "0.1.0"
