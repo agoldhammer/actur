477eff09a0dcbd6301e4a65e38f2a08d610a4f0b
Art Goldhammer <art.goldhammer@gmail.com>
2024-08-08 10:05:31 -0400

    reverted instruct

--- actur/categorize.py
+++ actur/categorize.py
@@ -42,7 +42,7 @@ def classify_by_title(title):
     messages.append(
         {"role": "user", "content": message},
     )
-    chat = client.completions.create(model="gpt-3.5-turbo-instruct", messages=messages)
+    chat = client.completions.create(model="gpt-3.5-turbo", messages=messages)
     reply = chat.choices[0].message.content  # type: ignore
     return reply
 
