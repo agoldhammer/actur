3b157a6740fa206659b49ee9b0bdbe5a8cdd31f7
Art Goldhammer <art.goldhammer@gmail.com>
2025-01-23 06:08:49 -0500

    bumped version to 0.2; reduced sample rate to 0.5

--- actur/utils/dbif.py
+++ actur/utils/dbif.py
@@ -8,11 +8,11 @@ sentry_sdk.init(
     dsn=rc.get_conf_by_key("sentry")["dsn"],
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
-    traces_sample_rate=1.0,
+    traces_sample_rate=0.5,
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
+    profiles_sample_rate=0.5,
 )
 
 _host: str | None = None
