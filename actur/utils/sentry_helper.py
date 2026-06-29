25380f4fa343799ed719bc6d14670b11987d160d
Art Goldhammer <art.goldhammer@gmail.com>
2026-06-29 09:25:05 -0400

    fixed sentry initialization

--- actur/utils/sentry_helper.py
+++ actur/utils/sentry_helper.py
@@ -2,13 +2,25 @@ import sentry_sdk
 
 from actur.config import readconf as rc
 
+_sentry_initialized = False
 
 def init_sentry():
-    sentry_sdk.init(
-        dsn = rc.get_conf_by_key("sentry")["dsn"],
-        traces_sample_rate = rc.get_conf_by_key("sentry")["sample_rate"],
-        profiles_sample_rate = rc.get_conf_by_key("sentry")["sample_rate"],
-    )
+    dsn = rc.get_conf_by_key("sentry")["dsn"]
+    sample_rate = rc.get_conf_by_key("sentry")["sample_rate"]
+    if dsn and sample_rate:
+        sentry_sdk.init(
+            dsn = dsn,
+            traces_sample_rate = sample_rate,
+            profiles_sample_rate = sample_rate
+        )
+        global _sentry_initialized
+        _sentry_initialized = True
+        print("Sentry initialized.")
+    else:
+        print("Sentry not initialized due to missing configuration.")
     
 def sentry_output(msg):
-    sentry_sdk.capture_message(msg)
+    if _sentry_initialized:
+        sentry_sdk.capture_message(msg)
+    else:
+        print(f"Sentry not initialized, cannot capture message {msg}.")
